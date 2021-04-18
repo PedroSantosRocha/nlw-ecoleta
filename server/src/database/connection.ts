9ed7ceb4752a429e8b04fabe15a1c4b3f7ab5866
Pedro Santos <pedrosantos.ti.web@gmail.com>
2021-04-18 20:40:07 -0300

    Alterações default

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -9,12 +9,4 @@ const connection = knex({
     useNullAsDefault: true,
 });
 
-// const connection = knex({
-//     client: 'sqlite3',
-//     connection: {
-//         filename: path.resolve(__dirname, 'database.sqlite'),
-//     },
-//     useNullAsDefault: true,
-// });
-
 export default connection;
