83a4687c017393dd6f330f8d1304cd29b31713f3
Pedro Santos <pedrosantos.ti.web@gmail.com>
2021-06-03 01:05:53 -0300

    proj andamento

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -9,4 +9,12 @@ const connection = knex({
     useNullAsDefault: true,
 });
 
+const connectionT = knex({
+    client: 'sqlite3',
+    connection: {
+        filename: path.resolve(__dirname, 'database.sqlite'),
+    },
+    useNullAsDefault: true,
+});
+
 export default connection;
